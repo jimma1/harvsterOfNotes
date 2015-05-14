d88a9a76e432d04ead503af4d89a480b74510d01
Jim Melby <jim.e.melby@hiof.no>
2015-05-14 15:45:36 +0200

    Migrated new model to DB

--- kvittr/notes/models.py
+++ kvittr/notes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Note(models.Model):
+	label = models.CharField(max_length = 30)
+	body = models.TextField()
+	# populate field with time of note creation
+	timestamp = models.DateTimeField(auto_now_add=True)
+
+	def __unicode__(self):
+		return self.label
