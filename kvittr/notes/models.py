167536209b3ace62fe6ee171398cbaf18608e21e
Jim Melby <jim.e.melby@hiof.no>
2015-05-15 09:58:45 +0200

    Added slugfield for url search on tags

--- kvittr/notes/models.py
+++ kvittr/notes/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+'''
+Searching in tags with GET request to a special crafted url.
+Must use slug which only accept letters, numbers, underscores and hyphens
+'''
 
 # Create your models here.
 class Note(models.Model):
@@ -14,6 +18,7 @@ class Note(models.Model):
 
 class Tag(models.Model):
 	label = models.CharField(max_length = 30)
+	slug = models.SlugField(max_length=30)
 
 	def __unicode__(self):
 		return self.label
