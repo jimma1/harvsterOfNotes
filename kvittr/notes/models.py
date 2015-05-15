3533ae744c7d35f22befa2ba5f606d42c80d0c7f
Jim Melby <jim.e.melby@hiof.no>
2015-05-15 09:23:39 +0200

    Added model for Tags

--- kvittr/notes/models.py
+++ kvittr/notes/models.py
@@ -6,6 +6,14 @@ class Note(models.Model):
 	body = models.TextField()
 	# populate field with time of note creation
 	timestamp = models.DateTimeField(auto_now_add=True)
+	# a note may have zero or many tags
+	tags = models.ManyToManyField('Tag', related_name='notes')
+
+	def __unicode__(self):
+		return self.label
+
+class Tag(models.Model):
+	label = models.CharField(max_length = 30)
 
 	def __unicode__(self):
 		return self.label
