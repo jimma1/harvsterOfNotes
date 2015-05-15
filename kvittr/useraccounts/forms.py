06a58bf6189604bb5a0786aca5b1bfecafc0e52b
Jim Melby <jim.e.melby@hiof.no>
2015-05-15 21:22:08 +0200

    Changed super(form) so self becomes an instance of the type.

--- kvittr/useraccounts/forms.py
+++ kvittr/useraccounts/forms.py
@@ -54,7 +54,7 @@ class UserupdateForm(forms.ModelForm):
 			)
 
 	def save(self, commit=True):
-		user = super(RegistrationForm, self).save(commit=False)
+		user = super(UserupdateForm, self).save(commit=False)
 		user.first_name = self.cleaned_data['first_name']
 		user.last_name= self.cleaned_data['last_name']
 		user.email = self.cleaned_data['email']
