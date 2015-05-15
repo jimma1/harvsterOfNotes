d707cc5a5b16f1eaccef79155c9d5fde63aaf276
Jim Melby <jim.e.melby@hiof.no>
2015-05-15 09:27:31 +0200

    Created form for Tag model

--- kvittr/notes/forms.py
+++ kvittr/notes/forms.py
@@ -1,8 +1,13 @@
 # Modelform interacts fast and easy with DB through defined models
 from django import forms
-from notes.models import Note
+from notes.models import Note, Tag
 
 class NoteForm(forms.ModelForm):
 	class Meta:
 		model = Note
-		fields = ('label', 'body')
+		fields = ('label', 'body', 'tags')
+
+class TagForm(forms.ModelForm):
+	class Meta:
+		model = Tag
+		fields =('label',)
