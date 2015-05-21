2ffb457877c69041eaa6561470dce268ae46b1ac
Jim Melby <jim.e.melby@hiof.no>
2015-05-21 10:11:00 +0200

    Added pagination and tag functionality

--- kvittr/kvittr/views.py
+++ kvittr/kvittr/views.py
@@ -2,10 +2,25 @@
 Added this view so that the core part of the project handels the home part of site
 '''
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-from notes.models import Note
+from notes.models import Note, Tag
 
 def home_view(request):
 	notes = Note.objects.all().order_by('-timestamp')
-	context = {'notes': notes}
+	# Pagination
+	page_number = request.GET.get('page')
+	paginator = Paginator(notes, 5)
+	try:
+		notes = paginator.page(page_number)
+	except PageNotAnInteger:
+		notes = paginator.page(1)
+	except EmptyPage:
+		notes = paginator.page(paginator.num_pages)
+
+	tags = Tag.objects.all()
+	context = {
+		'notes': notes,
+		'tags': tags
+		}
 	return render(request, 'home.html', context)
