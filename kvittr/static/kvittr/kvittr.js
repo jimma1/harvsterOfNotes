a24be7cd09d9f110705750545ea5dbe1ef023d43
Jim Melby <jim.e.melby@hiof.no>
2015-05-29 17:41:50 +0200

    Removed like functionality for detail page of single note

--- kvittr/static/kvittr/kvittr.js
+++ kvittr/static/kvittr/kvittr.js
@@ -2,39 +2,9 @@ $(document).ready(function(){
     /*
     * Get crsf in cookie. Possible with jquery.cookie.js. 
     * Send token with ajax in header to verify.
-    * First part of code works on detailnote.html
     */ 
 	var csrftoken = $.cookie('csrftoken');
-	// increase number of likes
- 	$("#increase_num_likes_thumb").click(function(event){
- 		event.preventDefault();
- 		$.ajax({
- 			method: "POST",
- 			url: $('#num_likes_url').val(),
- 			headers: { 'X-CSRFToken': $.cookie('csrftoken') }
- 		})
- 		.done(function(data){
- 			var num_likes_updated = data['num_likes_updated'];
- 			$("#num_likes_div").html(num_likes_updated)
- 		})
- 	});
-    // descreasing likes    
-    $("#decrease_num_likes_thumb").click(function(event){
-        event.preventDefault();
-        $.ajax({
-            method: "POST",
-            url: $('#decrease_num_likes_url').val(),
-            headers: { 'X-CSRFToken': $.cookie('csrftoken') }
-        })
-        .done(function(data){
-            var num_likes_updated = data['num_likes_updated'];
-            $("#num_likes_div").html(num_likes_updated)
-        })
-    });
-
-    /*
-    * Increasing likes on index.html
-    */
+    // increasing
     $(".class_increase_button").click(function(event){
         event.preventDefault();
         var note_id = $(this).data("noteid");
@@ -50,9 +20,7 @@ $(document).ready(function(){
             $(element_id).html(num_likes_updated)
         });
     });
-    /*
-    * Decreasing likes on index.html
-    */
+    // decreasing
     $(".class_decrease_button").click(function(event){
         event.preventDefault();
         var note_id = $(this).data("noteid");
