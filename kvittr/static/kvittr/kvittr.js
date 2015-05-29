ede7f36c397a01a64c6780e43c128bff8229aa21
Jim Melby <jim.e.melby@hiof.no>
2015-05-29 17:38:15 +0200

    Initialized decreasing likes on all notes on index.html

--- kvittr/static/kvittr/kvittr.js
+++ kvittr/static/kvittr/kvittr.js
@@ -50,7 +50,24 @@ $(document).ready(function(){
             $(element_id).html(num_likes_updated)
         });
     });
-
+    /*
+    * Decreasing likes on index.html
+    */
+    $(".class_decrease_button").click(function(event){
+        event.preventDefault();
+        var note_id = $(this).data("noteid");
+        var href = "/notes/" + note_id + "/decrease_num_likes/"; 
+        $.ajax({
+            method: "POST",
+            url: href,
+            headers: { 'X-CSRFToken': $.cookie('csrftoken') }
+        })
+        .done(function(data){
+            var num_likes_updated = data['num_likes_updated'];
+            var element_id = "#id_increase_" + note_id;
+            $(element_id).html(num_likes_updated)
+        });
+    });
 });
 
 
