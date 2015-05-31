6fab0cc337402dd5765d860abb38e9a934381419
Jim Melby <jim.e.melby@hiof.no>
2015-05-31 20:33:09 +0200

    Removed unused code for deleting tags.

--- kvittr/notes/views.py
+++ kvittr/notes/views.py
@@ -8,9 +8,7 @@ from django.http import JsonResponse
 
 from notes.models import Note, Tag
 from notes.forms import NoteForm, TagForm
-#from useraccounts.models import Member
 
-# Create your views here.
 '''
 Code is repeated in add_tagg and add_note, and pagination is repeated two places. One other solution for this is to write class based views.
 Since there is just this block which is repeated I do not prioritize to learn class based views now.
@@ -60,20 +58,12 @@ def tag_search(request, **kwargs):
 		'tag': tag,
 		'tags': tags
 		}
-
 	return render(request, 'notes/tagsearch.html', context)
 
 def add_tag(request):
-	#fetch tag by id to manipulate posted notes and pass the instance of posted note to form
-	id = request.GET.get('id', None)
-	if id is not None:
-		tag = get_object_or_404(Tag, id=id)
-	else:
-		tag = None
-
 	if request.method == 'POST':
 		# new tag which is slugifyed by Django, due to url search on tags
-		form = TagForm(request.POST, instance=tag)
+		form = TagForm(request.POST)
 		if form.is_valid():
 			t = form.save(commit=False)
 			t.slug = slugify(t.label)
@@ -81,12 +71,8 @@ def add_tag(request):
 			messages.add_message(request, messages.INFO, 'Tag Added')
 			return HttpResponseRedirect(reverse('notes:index'))
 	else:
-		form = TagForm(instance=tag)
-		context = {
-			'form': form,
-		 	'tag': tag
-		 	}
-
+		form = TagForm()
+		context = {'form': form}
 	return render(request, 'notes/addtag.html', context)
 
 def add_note(request):
@@ -99,7 +85,6 @@ def add_note(request):
 
 	# get the logged in user
 	author = request.user
-
 	if request.method == 'POST':
 		# delete note
 		# is there a variable named delete in html file
@@ -107,7 +92,6 @@ def add_note(request):
 			note.delete()
 			messages.add_message(request, messages.INFO, 'Note Deleted!')
 			return HttpResponseRedirect(reverse('notes:index'))
-		
 		# new note
 		form = NoteForm(request.POST, instance=note)
 		if form.is_valid():
@@ -127,7 +111,6 @@ def add_note(request):
 			'form': form,
 		 	'note': note
 		 	}
-
 	return render(request, 'notes/addnote.html', context)
 
 def index_view(request):
