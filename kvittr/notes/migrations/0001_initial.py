60636f9e822a6cfedc0840a06d6a7f1cea7b17e6
Jim Melby <jim.e.melby@hiof.no>
2015-06-03 07:49:33 +0200

    Removed old migrations and added one new opp to date migration

--- kvittr/notes/migrations/0001_initial.py
+++ kvittr/notes/migrations/0001_initial.py
@@ -2,11 +2,13 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
@@ -14,9 +16,24 @@ class Migration(migrations.Migration):
             name='Note',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('label', models.CharField(max_length=30)),
-                ('body', models.TextField()),
+                ('label', models.CharField(max_length=15)),
+                ('body', models.TextField(max_length=333)),
                 ('timestamp', models.DateTimeField(auto_now_add=True)),
+                ('num_likes', models.PositiveIntegerField(default=0)),
+                ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
+        ),
+        migrations.CreateModel(
+            name='Tag',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('label', models.CharField(max_length=20)),
+                ('slug', models.SlugField(max_length=20)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='note',
+            name='tags',
+            field=models.ManyToManyField(related_name='notes', to='notes.Tag'),
         ),
     ]
